69817bfda7ca45c6ba66f0f0b4f93bab5053182b
Nicole <nhswong@gmail.com>
2017-08-15 00:58:28 -0700

    fix url

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
-from django.contrib import admin
 
-from .import views
+from . import views
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
     url(r'^$', views.index, name='index'),
 ]
